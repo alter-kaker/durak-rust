0a6f9d8d37be4fa3e22518fc6ea673dea4342894
Marc Trius <37342326+marc-trius@users.noreply.github.com>
2023-06-23 16:09:05 -0500

    refactor asset loading

--- src/assets.rs
+++ src/assets.rs
@@ -8,38 +8,32 @@ const CARD_W: usize = 71;
 const CARD_H: usize = 96;
 
 pub struct Assets {
-    sprites: HashMap<Card, Sprite>,
+    card_sprites: HashMap<Card, Sprite>,
 }
 
 impl Assets {
     pub fn sprites(&self) -> &HashMap<Card, Sprite> {
-        &self.sprites
+        &self.card_sprites
     }
 }
 
 pub fn load_assets() -> Result<Assets, GameError> {
-    let decoder = Decoder::new(File::open("assets/cards.png")?);
-    let mut reader = decoder.read_info()?;
-    let mut buf = vec![0; reader.output_buffer_size()];
-    let info = reader.next_frame(&mut buf)?;
-    let palette = reader.info().palette.as_ref().unwrap().as_ref();
+    let (bytes, width) = load_png("assets/cards.png")?;
+    let card_sprites: HashMap<Card, Sprite> = read_cards(&bytes, width as usize)?;
 
-    println!("{:?}x{:?}", info.color_type, info.color_type.samples());
-    println!("{:?}", info.bit_depth);
+    Ok(Assets {
+        card_sprites,
+    })
+}
 
-    let width = info.width;
-    let bytes: &Vec<u8> = &buf[..info.buffer_size()]
-        .iter()
-        .map(|i| *i as usize)
-        .flat_map(|i| palette[i * 3..(i * 3) + 3].to_vec())
-        .collect();
-    let sprites: HashMap<Card, Sprite> = (0..9)
+fn read_cards(bytes: &[u8], width: usize) -> Result<HashMap<Card, Sprite>, GameError> {
+    let result = (0..9)
         .flat_map(|rank| {
             (0..4).map(move |suit| {
                 let card_bytes: Vec<u8> = ((suit * CARD_H)..(suit * CARD_H) + CARD_H)
                     .flat_map(|y| {
                         ((rank * CARD_W)..(rank * CARD_W) + CARD_W).flat_map(move |x| {
-                            let idx = (x + (y * width as usize)) * 3;
+                            let idx = (x + (y * width)) * 3;
                             let mut pixel = bytes[idx..idx + 3].to_vec();
                             pixel.push(0xff);
                             pixel
@@ -61,5 +55,22 @@ pub fn load_assets() -> Result<Assets, GameError> {
         })
         .collect();
 
-    Ok(Assets { sprites })
+    Ok(result)
+}
+
+fn load_png(path: &str) -> Result<(Vec<u8>, u32), GameError> {
+    let decoder = Decoder::new(File::open(path)?);
+    let mut reader = decoder.read_info()?;
+    let mut buf = vec![0; reader.output_buffer_size()];
+    let info = reader.next_frame(&mut buf)?;
+    let palette = reader.info().palette.as_ref().unwrap().as_ref();
+
+    let width = info.width;
+    let bytes: Vec<u8> = buf[..info.buffer_size()]
+        .iter()
+        .map(|i| *i as usize)
+        .flat_map(|i| palette[i * 3..(i * 3) + 3].to_vec())
+        .collect();
+
+    Ok((bytes, width))
 }
